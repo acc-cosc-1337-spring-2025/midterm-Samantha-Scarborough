dc34340c1fbc5a7a8fc0d5d99e584f038309d685
Samantha-Scarborough <samantha.scarborough@g.austincc.edu>
2025-03-11 22:41:00 +0000

    Test question 02

--- test/question_test_2/question_tests_2.cpp
+++ test/question_test_2/question_tests_2.cpp
@@ -10,3 +10,14 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+
+#define CATCH_CONFIG_MAIN  
+#include "catch.hpp"  
+#include "temperature_conversion.h"  
+
+// Test cases for get_fahrenheit function
+TEST_CASE("Test Celsius to Fahrenheit conversion") {
+    REQUIRE(get_fahrenheit(37) == Approx(98.6).margin(0.01));
+    REQUIRE(get_fahrenheit(25) == Approx(77.0).margin(0.01));
+    REQUIRE(get_fahrenheit(0) == Approx(32.0).margin(0.01));
+}
