1f0d5f2edebf1b724870dcc42a921871f6927d27
Samantha-Scarborough <samantha.scarborough@g.austincc.edu>
2025-03-11 22:52:05 +0000

    Test question 03

--- test/question_test_3/question_tests_3.cpp
+++ test/question_test_3/question_tests_3.cpp
@@ -10,3 +10,20 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+#define CATCH_CONFIG_MAIN  // Enables Catch2's main function
+#include "catch.hpp"
+#include "fibonacci.h"  // Include the Fibonacci function prototype
+
+TEST_CASE("Test Fibonacci sequence generation") {
+    // Test case for get_fib_sequence(5)
+    REQUIRE(get_fib_sequence(5) == "0 1 1 2 3 5");
+
+    // Test case for get_fib_sequence(7)
+    REQUIRE(get_fib_sequence(7) == "0 1 1 2 3 5 8");
+
+	// Test case for get_fib_sequence(10)
+    REQUIRE(get_fib_sequence(10) == "0 1 1 2 3 5 8 13 21 34 55");
+
+	// Test case for get_fib_sequence(12)
+	REQUIRE(get_fib_sequence(12) == "0 1 1 2 3 5 8 13 21 34 55 89 144");
+}
